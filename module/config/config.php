27f9a74b9a197276e638c38fa27333472791027d
David Molineus <mail@netzmacht.de>
2014-05-07 16:27:27 +0200

    make sure that hook arrays exists

--- module/config/config.php
+++ module/config/config.php
@@ -3,6 +3,13 @@
 // hooks
 $GLOBALS['TL_HOOKS']['parseTemplate'][] = array('TemplateExtended\TemplateEngine', 'initialize');
 
+if(!isset($GLOBALS['TL_HOOKS']['parseFrontendTemplate'])) {
+	$GLOBALS['TL_HOOKS']['parseFrontendTemplate'] = array();
+}
+
+if(!isset($GLOBALS['TL_HOOKS']['parseBackendTemplate'])) {
+	$GLOBALS['TL_HOOKS']['parseBackendTemplate'] = array();
+}
 
 // TemplateExtended inheritance can be enabled by template black or whitelist or gobal for every template
 $GLOBALS['TEMPLATE_INHERITANCE'] = 'blacklist';
